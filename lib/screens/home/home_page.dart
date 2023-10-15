b8fcb3da5ddfd0ae777a642f05c1829696b632ac
Promlert Lovichit <promlert@gmail.com>
2023-10-15 17:58:40 +0700

    Populate UI with data from API

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -13,17 +13,31 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final _dio = Dio(BaseOptions(responseType: ResponseType.plain));
+  List<TodoItem>? _itemList;
+  String? _error;
 
   void getTodos() async {
-    final response =
-        await _dio.get('https://jsonplaceholder.typicode.com/todos');
-    debugPrint(response.data.toString());
-    List list = jsonDecode(response.data.toString());
-    List<TodoItem> itemList =
-        list.map((item) => TodoItem.fromJson(item)).toList();
-    itemList.forEach((elm) {
-      debugPrint(elm.title);
-    });
+    try {
+      setState(() {
+        _error = null;
+      });
+
+      // await Future.delayed(const Duration(seconds: 3), () {});
+
+      final response =
+          await _dio.get('https://jsonplaceholder.typicode.com/todos');
+      debugPrint(response.data.toString());
+      // parse
+      List list = jsonDecode(response.data.toString());
+      setState(() {
+        _itemList = list.map((item) => TodoItem.fromJson(item)).toList();
+      });
+    } catch (e) {
+      setState(() {
+        _error = e.toString();
+      });
+      debugPrint('เกิดข้อผิดพลาด: ${e.toString()}');
+    }
   }
 
   @override
@@ -34,6 +48,45 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Widget body;
+
+    if (_error != null) {
+      body = Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(_error!),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              getTodos();
+            },
+            child: const Text('RETRY'),
+          )
+        ],
+      );
+    } else if (_itemList == null) {
+      body = const Center(child: CircularProgressIndicator());
+    } else {
+      body = ListView.builder(
+          itemCount: _itemList!.length,
+          itemBuilder: (context, index) {
+            var todoItem = _itemList![index];
+            return Card(
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(children: [
+                      Expanded(child: Text(todoItem.title)),
+                      Checkbox(
+                          value: todoItem.completed,
+                          onChanged: (newValue) {
+                            setState(() {
+                              todoItem.completed = newValue!;
+                            });
+                          })
+                    ])));
+          });
+    }
+
+    return Scaffold(body: body);
   }
 }
